dfedfd008b5455dc401c6007035961eaee1acec6
NO-Group <n.ogroup@yahoo.com>
2026-07-16 00:30:59 +0100

    Add script to install and configure Flutter
    This script installs Flutter in the user's home directory, updates it if already installed, and adds Flutter to the system PATH. It also runs 'flutter doctor' and 'flutter precache' to set up the environment.

---
+++ .devcontainer/install_flutter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Install Flutter to a permanent location in the workspace
+FLUTTER_DIR="$HOME/flutter"
+
+if [ ! -d "$FLUTTER_DIR" ]; then
+  echo "Installing Flutter..."
+  git clone https://github.com/flutter/flutter.git -b stable $FLUTTER_DIR
+else
+  echo "Flutter already installed. Updating..."
+  cd $FLUTTER_DIR && git pull
+fi
+
+# Add Flutter to PATH permanently
+echo 'export PATH="$PATH:$HOME/flutter/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/flutter/bin"' >> ~/.zshrc
+
+# Accept licenses and pre-cache
+export PATH="$PATH:$HOME/flutter/bin"
+flutter doctor
+flutter precache
